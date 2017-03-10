b97fbcc4fbd3d30223dc027e032faa212351ecc7
sn446 <sn446@greenwich.ac.uk>
2017-03-10 16:33:40 +0000

    Update coordinator.php

--- public_html/coordinator.php
+++ public_html/coordinator.php
@@ -31,7 +31,7 @@ $sql1 = "select  id,username, name, date, feedback from uploads";
 <link href="CSS/bootstrap.css" rel="stylesheet" type="text/css" />
  <link href="CSS/style.css" rel="stylesheet" type="text/css" />
     
-    <head>
+    <head>  
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
